c5c2452c92e3043bec5aed31e847ed34a13949e7
Dylan Moore <dylanxmoore0@gmail.com>
2026-08-27 15:07:15 +0100

    feat: return each songs artist if supported file types, else throws an error

---
+++ README.md
@@ -0,0 +1,7 @@
+Python Music Organiser
+
+Error Handling:
+    "musicTypeError" - Please ensure the directory provided only contains supported files.
+
+Currently Supported Files:
+    "mp3"
--- music.py
+++ music.py
@@ -8,12 +8,28 @@ def get_music_location() -> str:
     return music_path
 
 def check_music(music_path):
+    # Loop over each file, checking its a supported file type
     for file in os.listdir(music_path):
         if file.lower().endswith(".mp3"):
-            print(f"Reading {file}...")
+            continue
+        else:
+            # If its an unsupported type, log it and throw an error
+            print(f"{file} is not a supported file type.")
+            print("Please ensure this directory only contains supported file types")
+            exit("musicTypeError")
+
+def get_music_info(music_path):
+    # Loop through songs, combining paths for TinyTag
+    for file in os.listdir(music_path):
+        full_path = os.path.join(music_path, file)
+
+        # List the Artists of each track in the music_path
+        tag: TinyTag = TinyTag.get(full_path)
+        print(f"{tag.artist}")
 
 def temp_main():
     music_path = get_music_location()
     check_music(music_path)
+    get_music_info(music_path)
 
 temp_main()
